d01ac19dc13a974548f2f1cc1284bc9b6ad84bc8
Yan Fabricio <yan.minduca@gmail.com>
2026-04-03 13:10:36 -0300

    main alterado

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// 🔥 IMPORTANTE: usar HashRouter no GitHub Pages
+import { HashRouter } from 'react-router-dom'
+
+// (opcional) estilos globais
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
